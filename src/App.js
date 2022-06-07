809d98cef0b0dedf9bcd597fc9e0897f85d6721b
Dedpool1998 <msmsmanishkumar64@gmail.com>
2022-06-07 23:22:36 +0530

    css added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 // import logo from './logo.svg';
-import './App.css';
-import Counter from './Components/Counter';
+import "./App.css";
+import Counter from "./Components/Counter";
 
 function App() {
   return (
-    <div className="App">
+    <div
+      style={{
+        border: "1px solid red",
+        width: "30%",
+        margin: "auto",
+        height: "300px",
+        marginTop: "50px",
+        boxShadow:
+          "rgb(85, 91, 255) 0px 0px 0px 3px, rgb(31, 193, 27) 0px 0px 0px 6px, rgb(255, 217, 19) 0px 0px 0px 9px, rgb(255, 156, 85) 0px 0px 0px 12px, rgb(255, 85, 85) 0px 0px 0px 15px",
+      backgroundColor:"yellowgreen"
+        }}
+      className="App"
+    >
       <Counter></Counter>
     </div>
   );
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -16,23 +16,76 @@ const Counter = () => {
   const [value, setValue] = React.useState();
 
   const handleOnChange = (e) => {
-    setValue(+(e.target.value));
+    setValue(+e.target.value);
   };
   return (
     <div>
       <h1>Counter: {count}</h1>
       <div>
-        <button onClick={() => dispatch(add())}>+</button>
-        <button onClick={() => dispatch(substract())}>-</button>
+        <button
+          style={{ width: "40px", height: "40px", cursor: "pointer" }}
+          onClick={() => dispatch(substract())}
+        >
+          -
+        </button>
+        <button
+          style={{
+            width: "40px",
+            height: "40px",
+            marginLeft: "10px",
+            cursor: "pointer",
+          }}
+          onClick={() => dispatch(add())}
+        >
+          +
+        </button>
       </div>
 
       <div>
-        <input type="number" onChange={(e) => handleOnChange(e)} />
-
-        <button onClick={() => dispatch(addition(value))}>+</button>
-        <button onClick={() => dispatch(substraction(value))}>-</button>
-        <button onClick={() => dispatch(multiplication(value))}>*</button>
-        <button onClick={() => dispatch(division(value))}>/</button>
+        <input
+          style={{ marginTop: "30px", textAlign: "center" }}
+          type="number"
+          onChange={(e) => handleOnChange(e)}
+          placeholder={"Number"}
+        />
+        <div
+          style={{
+            margin: "auto",
+            border: "1px solid red",
+            marginTop: "30px",
+            display: "flex",
+            justifyContent: "space-evenly",
+            width: "42%",
+            backgroundColor:"pink",
+            height:"60px",
+            alignItems:"center"
+          }}
+        >
+          <button
+            style={{ width: "40px", height: "40px", cursor: "pointer" ,borderRadius:"50px",border:"none"}}
+            onClick={() => dispatch(addition(value))}
+          >
+            +
+          </button>
+          <button
+            style={{ width: "40px", height: "40px", cursor: "pointer" ,borderRadius:"50px",border:"none"}}
+            onClick={() => dispatch(substraction(value))}
+          >
+            -
+          </button>
+          <button
+            style={{ width: "40px", height: "40px", cursor: "pointer" ,borderRadius:"50px",border:"none"}}
+            onClick={() => dispatch(multiplication(value))}
+          >
+            *
+          </button>
+          <button
+            style={{ width: "40px", height: "40px", cursor: "pointer",borderRadius:"50px",border:"none" }}
+            onClick={() => dispatch(division(value))}
+          >
+            /
+          </button>
+        </div>
       </div>
     </div>
   );
